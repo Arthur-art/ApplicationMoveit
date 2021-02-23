d982cfec26bb53504d656ad9973b2ce4c369ba57
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-23 17:49:51 -0300

    creating components Profile and css-module

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return (
+        <div>
+            <img src="https://github.com/arthur-art.png" alt="Arthur"/>
+            <div>
+                <strong>Arthur Teixeira</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,19 +1,20 @@
-import {ExpirienceBar} from '../components/ExperienceBar'
-import Head from 'next/head'
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
 
 export default function Home() {
   return (
     <div className="container">
+      <ExperienceBar />
 
-    <Head>
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
 
-    <link rel="preconnect" href="https://fonts.gstatic.com"/>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Nunito:ital,wght@0,400;0,600;1,800&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
+        </div>
+      </section>
 
-    </Head>
-
-    <ExpirienceBar />
-    
-  </div>
-  )
+    </div>
+  );
 }
