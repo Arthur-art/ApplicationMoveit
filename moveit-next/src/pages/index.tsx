cd28c51c4746d9669157fc4b95a7b40ca979aed7
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-23 21:46:26 -0300

    Creating hook countdown

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -4,9 +4,16 @@ import styles from '../styles/pages/Home.module.css'
 import {CompletedChallenges} from '../components/CompletedChallenges';
 import {Countdown} from '../components/Countdown';
 
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
