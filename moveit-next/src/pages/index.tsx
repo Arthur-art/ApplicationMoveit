07862e41f57a09f467bf947fa53451f76bd87e81
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-24 17:30:20 -0300

    Creating components ChallengeBox

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,15 +1,14 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from '../styles/pages/Home.module.css'
-import {CompletedChallenges} from '../components/CompletedChallenges';
-import {Countdown} from '../components/Countdown';
-
-import Head from 'next/head';
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
+import Head from "next/head";
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
       <Head>
         <title>Início | move.it</title>
       </Head>
@@ -23,10 +22,9 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
-
     </div>
   );
 }
