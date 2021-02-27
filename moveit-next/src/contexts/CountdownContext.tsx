ddce10cecd729e3c6aeb89626ae9329b485359be
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-27 16:24:57 -0300

    Deploy - finishing project

--- moveit-next/src/contexts/CountdownContext.tsx
+++ moveit-next/src/contexts/CountdownContext.tsx
@@ -27,7 +27,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFineshed] = useState(false);
 
@@ -38,7 +38,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setHasFineshed(false);
     setIsActive(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
   }
 
   function startCountdown() {
